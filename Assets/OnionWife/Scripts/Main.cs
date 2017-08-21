47ad1ef3db27636d057f4348fbac5dda7b4bab96
Marcel Barros Aparici <barros.marcel92@gmail.com>
2017-08-21 20:48:17 +0200

    feat: bunnies with explosion and onion

---
+++ Assets/OnionWife/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+  private Main Initialize() {
+    return this;
+  }
+
+  private Main InitalizeBunnySpawner() {
+
+    return this;
+  }
+  void Start() {
+    Initialize();
+  }
+
+  private BunnySpawner bunnySpawner;
+}
